dc86280bbfae072acacaa7be2f282632a6d6840d
Elle256 <thanhthutran2006@gmail.com>
2025-10-11 21:23:00 +0700

    Add files via upload

---
+++ GA.py
@@ -0,0 +1,151 @@
+#import thư viện
+import numpy as np
+import random
+import copy
+import matplotlib.pyplot as plt
+
+# ----- Định nghĩa bài toán -----
+class Problem:
+    def __init__(self):
+        pass
+
+# Hàm giải mã: chuyển nhiễm sắc thể -> giá trị x thật
+def decode(chromosome, problem: Problem):
+    pass
+
+# Hàm đánh giá (fitness function)
+def get_fitness(x):
+    pass
+# ----- Lớp cá thể -----
+class Individual:
+    def __init__(self):
+        self.chromosome = None
+        self.fitness = None
+
+    def genIndi(self, problem : Problem):
+        self.chromosome = np.random.uniform(0.0, 1.0, 10)
+        # pass
+    
+    def cal_fitness(self, problem):
+        self.fitness = get_fitness(self.chromosome)
+    
+    def clone(self):
+        return copy.deepcopy(self)
+    
+    def __repr__(self):
+        return f"chromosome={self.chromosome}, fitness={self.fitness}" 
+ 
+# ----- Lai ghép SBX -----
+def crossover(parent1, parent2, eta = 2.0):
+    off1 = Individual()
+    off2 = Individual()
+    r = np.random.rand()
+    if (r <= 0.5):
+        beta = (2*r)**(1.0/(eta + 1))
+    else:
+        beta = (1.0/(2*(1 - r)))**(1.0/(eta + 1))
+    p1 = parent1.chromosome
+    p2 = parent2.chromosome
+    c1 = 0.5 * ((1 + beta) * p1 + (1 - beta) * p2)
+    c2 = 0.5 * ((1 - beta) * p1 + (1 + beta) * p2)
+    c1 = np.clip(c1, 0.0, 1.0)
+    c2 = np.clip(c2, 0.0, 1.0)
+    off1.chromosome = c1
+    off2.chromosome = c2
+    return off1.clone(), off2.clone()
+
+# ----- Đột biến -----
+def mutation(individual, eta = 20.0):
+    indi = individual.clone()
+    chr = indi.chromosome
+    for i in range(chr.size):
+        mu = np.random.rand()
+        if (mu <= 0.5):
+            delta = (2 * mu)**(1.0/(1 + eta)) - 1
+            chr[i] = chr[i] + delta * chr[i]
+        else:
+            delta = 1 - (2 - 2*mu)**(1.0/(1 + eta))
+            chr[i] = chr[i] + delta * (1 - chr[i])
+            
+    chr = np.clip(chr, 0.0, 1.0)
+    indi.chromosome = chr
+    return indi.clone()
+class Population:
+    def __init__(self, pop_size, problem : Problem):
+        self.pop_size = pop_size
+        self.list_indi = []
+        self.problem = problem
+    
+    def genPop(self):
+        for i in range(self.pop_size):
+            indi = Individual()
+            indi.genIndi(self.problem)
+            indi.cal_fitness(self.problem)
+            self.list_indi.append(indi)
+
+    def __repr__(self):
+        pass
+def selection(list, k = 2):
+    tour1 = random.sample(list, k)
+    tour2 = random.sample(list, k)
+    x = max(tour1, key=lambda indi: indi.fitness)
+    y = max(tour2, key=lambda indi: indi.fitness)
+    return x.clone(), y.clone()
+def survival_selection(list, pop_size):
+    list = sorted(list, key=lambda indi: indi.fitness, reverse=True)
+    list = list[0: pop_size]
+    return list
+def GA(problem, pop_size, max_gen, p_c, p_m):
+    pop = Population(pop_size, problem)
+    pop.genPop()
+    history = []
+    for i in range(max_gen):
+        child = []
+        while (len(child) < pop_size):
+            p1, p2 = selection(pop.list_indi)
+            if np.random.rand() <= p_c:
+                c1, c2 = crossover(p1, p2, problem)
+                c1.cal_fitness(problem)
+                c2.cal_fitness(problem)
+                child.append(c1)
+                child.append(c2)
+            if np.random.rand() <= p_m:
+                p1 = mutation(p1)
+                p2 = mutation(p2)
+                p1.cal_fitness(problem)
+                p2.cal_fitness(problem)
+                child.append(p1)
+                child.append(p2)
+        pop.list_indi = survival_selection(pop.list_indi + child, pop_size)
+        history.append(pop.list_indi[0].fitness)
+    solution = pop.list_indi[0]
+    return history, solution
+# setup
+problem = Problem()
+
+pop_size = 200
+max_gen = 2000
+Pc = 0.9
+Pm = 0.2
+
+# start
+fitness_history, solution = GA(problem, pop_size, max_gen, Pc, Pm)
+
+#show
+for i in range(len(fitness_history)):
+    print(f"Generation {i}, bestfitness = {fitness_history[i]:.2f}")
+#show
+np.set_printoptions(precision=2, suppress=True)
+print("solution:")
+print(decode(solution.chromosome, problem))
+print(f"{solution.fitness:.2f}")
+generations = list(range(len(fitness_history)))
+plt.figure(figsize=(10, 5))
+plt.plot(generations, fitness_history, marker='o', linestyle='-', color='b', label='Best Fitness')
+
+plt.xlabel("Generation")
+plt.ylabel("Best Fitness")
+plt.title("Fitness Progress Over Generations")
+plt.legend()
+plt.grid(True)
+plt.show()
